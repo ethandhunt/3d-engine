75acb94ab07f0a1c87e8849352e0b1f7ae68bfe8
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-07-31 13:13:48 +1200

    Better performance, bigger map

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import sys
+import time
 
 pygame.init()
 WIDTH = 500
@@ -12,6 +13,18 @@ BLACK = (0, 0, 0)
 def outTermMap():
     print('\n'.join(' '.join(('#' * y) + (' ' * abs(y-1)) for y in x) for x in MAP))
 
+def drawText(text, size, colour, pos, font='courier', centered=False, updateScreen=False, antiAlias=True, background=(0,0,0)):
+    myfont = pygame.font.SysFont(font, size)
+    textsurface = myfont.render(text, antiAlias, colour, background)
+    x = pos[0]
+    y = pos[1]
+    if centered:
+        window.blit(textsurface, (x - myfont.size(text)[0]//2, y - myfont.size(text)[1]//2))
+    else:
+        window.blit(textsurface, (x, y))
+    if updateScreen:
+        pygame.display.update()
+    return myfont.size(text)
 
 with open('map.txt') as f:
     rawMap = f.read()
@@ -31,7 +44,6 @@ def getMap(x, y):
     return MAP[max(min(round(x), len(MAP) - 1), 0)][max(min(round(y), len(MAP[0]) - 1), 0)]
 
 def render():
-    pygame.draw.rect(window, BLACK, ((0, 0), (WIDTH, HEIGHT)))
     FOV = math.radians(60)
     castResolution = 20
     for i in range(WIDTH):
@@ -41,19 +53,23 @@ def render():
         # make dist not = 0. Ever
         rayX += math.sin(rayDir) / castResolution
         rayY += math.cos(rayDir) / castResolution
-        while math.dist((x, y), (rayX, rayY)) < 100 and not getMap(rayX, rayY):
-            rayX += math.sin(rayDir) / castResolution
-            rayY += math.cos(rayDir) / castResolution
+        iteration = 0
+        while iteration < 100 and not getMap(rayX, rayY):
+            rayX += math.sin(rayDir) / castResolution * iteration/10
+            rayY += math.cos(rayDir) / castResolution * iteration/10
+            iteration += 1
 
         lineHeight = 1/max(rayDist(rayX, rayY), 1) * HEIGHT
         pygame.draw.line(window, (min(1/rayDist(rayX, rayY), 1)*255, min(1/rayDist(rayX, rayY), 1)*255, min(1/rayDist(rayX, rayY), 1)*255), (i, (HEIGHT - lineHeight)/2), (i, HEIGHT + (lineHeight - HEIGHT)/2))
-    pygame.display.update()
 
 outTermMap()
 
 dir = 0
 walkSpeed = 0.1
+
+startTime = time.time()
 while True:
+    pygame.draw.rect(window, BLACK, ((0, 0), (WIDTH, HEIGHT)))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -72,3 +88,6 @@ while True:
         if not getMap(x - math.sin(dir) * walkSpeed, y - math.cos(dir) * walkSpeed):
             x -= math.sin(dir) * walkSpeed
             y -= math.cos(dir) * walkSpeed
+    drawText(f'FPS: {int(1/(time.time() - startTime))}', 12, (255, 255, 0), (0, 0))
+    startTime = time.time()
+    pygame.display.update()
