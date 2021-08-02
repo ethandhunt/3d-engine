fc769be7e0207768c8554e62c0769f49ca5c6469
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-08-02 20:11:10 +1200

    Revert "Mouse Rotation and WASD movement"
    This reverts commit b91554c5000dbaee81fc011825f5f31a58ba496a.

--- main.py
+++ main.py
@@ -2,73 +2,11 @@ import pygame
 import math
 import sys
 import time
-<<<<<<< HEAD
-<<<<<<< HEAD
-import socket
-import threading
-
-def send(conn, msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message)
-
-def rcv(conn):
-    while True:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            return conn.recv(msg_length).decode(FORMAT)
-
-class connection:
-    def __init__(self, conn):
-        self.conn = conn
-    
-    def rcv(self):
-        return rcv(self.conn)
-    
-    def send(self, msg):
-        send(self.conn, msg)
-
-HEADER = 64
-FORMAT = 'utf-8'
-IP = socket.gethostbyname(input('Enter server name: '))
-PORT = 55555
-ADDR = (IP, PORT)
-print(IP, PORT)
-print('Connecting...')
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect(ADDR)
-SERVER = connection(conn)
-playerPositions = []
-
-def playerPos():
-    global playerPositions
-    global exit
-    while True:
-        SERVER.send('GETPLAYERS')
-        unparsedString = SERVER.rcv()
-        if unparsedString == '':
-            SERVER.send(f'{x}:{y}')
-            continue
-        playerPositions = [[float(b) for b in a.split(':')] for a in unparsedString.split(',')]
-        SERVER.send(f'{x}:{y}')
-        if exit:
-            SERVER.send("QUIT")
-            sys.exit()
-=======
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
-=======
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
 
 pygame.init()
 WIDTH = 500
 HEIGHT = 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.mouse.set_visible(False)
-pygame.event.set_grab(True)
 
 BLACK = (0, 0, 0)
 
@@ -108,18 +46,8 @@ def getMap(x, y):
 def render():
     FOV = math.radians(60)
     castResolution = 20
-<<<<<<< HEAD
-<<<<<<< HEAD
-    array = []
-    rayWidth = 10
-    for i in range(WIDTH//rayWidth):
-        rayDir = FOV/WIDTH/rayWidth * (i - WIDTH/2/rayWidth) + dir
-=======
-=======
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
     for i in range(WIDTH):
         rayDir = FOV/WIDTH * (i - WIDTH/2) + dir
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
         rayX = x
         rayY = y
         # make dist not = 0. Ever
@@ -133,55 +61,33 @@ def render():
 
         lineHeight = 1/max(rayDist(rayX, rayY), 1) * HEIGHT
         pygame.draw.line(window, (min(1/rayDist(rayX, rayY), 1)*255, min(1/rayDist(rayX, rayY), 1)*255, min(1/rayDist(rayX, rayY), 1)*255), (i, (HEIGHT - lineHeight)/2), (i, HEIGHT + (lineHeight - HEIGHT)/2))
-<<<<<<< HEAD
-<<<<<<< HEAD
-    if playerPositions == []: return
-    for player in playerPositions:
-        screenX = ((getAngle((x, y), player) - dir)/FOV)*WIDTH
-        print(screenX)
-        pygame.draw.circle(window, (255, 0, 0), (screenX, HEIGHT//2), max(rayDist(player[0], player[1]), 1) * HEIGHT)
-=======
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
-=======
->>>>>>> parent of 42cd706 (Added multiplayer, doesn't let you see people yet tho)
 
 outTermMap()
 
 dir = 0
 walkSpeed = 0.1
+
 startTime = time.time()
-print('Press esc to exit window')
 while True:
     pygame.draw.rect(window, BLACK, ((0, 0), (WIDTH, HEIGHT)))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
     render()
-    dir += math.radians(pygame.mouse.get_rel()[0])
-    dir = dir % math.radians(360)
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        if not getMap(x + math.sin(dir - math.radians(90)) * walkSpeed, y - math.cos(dir + math.radians(90)) * walkSpeed):
-            x -= math.sin(dir + math.radians(90)) * walkSpeed
-            y -= math.cos(dir + math.radians(90)) * walkSpeed
-    if keys[pygame.K_d]:
-        if not getMap(x + math.sin(dir + math.radians(90)) * walkSpeed, y + math.cos(dir + math.radians(90)) * walkSpeed):
-            x += math.sin(dir + math.radians(90)) * walkSpeed
-            y += math.cos(dir + math.radians(90)) * walkSpeed
-    if keys[pygame.K_w]:
+    if keys[pygame.K_LEFT]:
+        dir -= math.radians(10)
+    if keys[pygame.K_RIGHT]:
+        dir += math.radians(10)
+    if keys[pygame.K_UP]:
         if not getMap(x + math.sin(dir) * walkSpeed, y + math.cos(dir) * walkSpeed):
             x += math.sin(dir) * walkSpeed
             y += math.cos(dir) * walkSpeed
-    if keys[pygame.K_s]:
+    if keys[pygame.K_DOWN]:
         if not getMap(x - math.sin(dir) * walkSpeed, y - math.cos(dir) * walkSpeed):
             x -= math.sin(dir) * walkSpeed
             y -= math.cos(dir) * walkSpeed
-    drawText(f'FPS: {int(1/(time.time() - startTime))}', 12, (255, 255, 255), (0, 0))
+    drawText(f'FPS: {int(1/(time.time() - startTime))}', 12, (255, 255, 0), (0, 0))
     startTime = time.time()
     pygame.display.update()
